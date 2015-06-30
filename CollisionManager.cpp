84fcb9d21fce25c4078b442cf5381a5e90ebdb40
Adam <theadam@gmx.de>
2015-06-30 10:43:19 +0200

    collision detection class added

---
+++ CollisionManager.cpp
@@ -0,0 +1,22 @@
+/*
+ * CollisionManager.cpp
+ *
+ *  Created on: 30.06.2015
+ *      Author: Adam
+ */
+
+#include "CollisionManager.h"
+
+CollisionManager::CollisionManager() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CollisionManager::~CollisionManager() {
+	// TODO Auto-generated destructor stub
+}
+
+
+void collisionBox () {
+
+}q
---
+++ CollisionManager.h
@@ -0,0 +1,17 @@
+/*
+ * CollisionManager.h
+ *
+ *  Created on: 30.06.2015
+ *      Author: Adam
+ */
+
+#ifndef COLLISIONMANAGER_H_
+#define COLLISIONMANAGER_H_
+
+class CollisionManager {
+public:
+	CollisionManager();
+	virtual ~CollisionManager();
+};
+
+#endif /* COLLISIONMANAGER_H_ */
